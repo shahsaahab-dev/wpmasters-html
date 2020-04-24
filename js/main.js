3b45195664dc6a524b9f998c684be7e36447f5bf
Syed Naqi <naqishah2997@gmail.com>
2020-04-24 12:39:55 +0500

    Changes laptop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 // All the Custom Scripting Here.
+jQuery(document).ready(function ($) {
+
+
+    // Checking if Header LI has a child for mega menu then add i.fa.fa-angle-down
+
+    $(".header-menu li").each(function () {
+        var e = $(this);
+        if (e.children("ul").length > 0) {
+            e.addClass("has-submenu");
+            $(this).append('<i class="fa fa-angle-down"></i>');
+        }
+    });
+
+
+
+
+});
